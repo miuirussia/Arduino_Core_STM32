da1e24c49536b9353306b467eafbddabc4d139af
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2019-12-26 20:38:07 +0300

    Add board to boards.txt and fix formatting

--- variants/KDEVLAB_F411CE/variant.cpp
+++ variants/KDEVLAB_F411CE/variant.cpp
@@ -21,38 +21,38 @@ extern "C" {
 // This array allows to wrap Arduino pin number(Dx or x)
 // to STM32 PinName (PX_n)
 const PinName digitalPin[] = {
-    //PX_n, //Dx
-    // Left side
-    PB_9,  //D0
-    PB_8,  //D1
-    PB_7,  //D2
-    PB_6,  //D3
-    PB_5,  //D4
-    PB_4,  //D5
-    PB_3,  //D6
-    PA_15, //D7
-    PA_10, //D8
-    PA_9,  //D9
-    PA_8,  //D10
-    PB_15, //D11
-    PB_14, //D12
-    PB_13, //D13
-    PB_12, //D14
-    // Right side
-    PA_0,  //D15
-    PA_1,  //D16/A0
-    PA_2,  //D17/A1
-    PA_3,  //D18/A2
-    PB_0,  //D19/A3
-    PB_1,  //D20/A4
-    PB_10, //D21/A5
+  //PX_n, //Dx
+  // Left side
+  PB_9,  //D0
+  PB_8,  //D1
+  PB_7,  //D2
+  PB_6,  //D3
+  PB_5,  //D4
+  PB_4,  //D5
+  PB_3,  //D6
+  PA_15, //D7
+  PA_10, //D8
+  PA_9,  //D9
+  PA_8,  //D10
+  PB_15, //D11
+  PB_14, //D12
+  PB_13, //D13
+  PB_12, //D14
+  // Right side
+  PA_0,  //D15
+  PA_1,  //D16/A0
+  PA_2,  //D17/A1
+  PA_3,  //D18/A2
+  PB_0,  //D19/A3
+  PB_1,  //D20/A4
+  PB_10, //D21/A5
 
-    PA_4, // SPI1_SS
-    PA_5, // SPI1_SCK
-    PA_6, // SPI1_MISO
-    PA_7, // SPI1_MOSI
-    
-    PC_13 // NEOPIXEL
+  PA_4, // SPI1_SS
+  PA_5, // SPI1_SCK
+  PA_6, // SPI1_MISO
+  PA_7, // SPI1_MOSI
+
+  PC_13 // NEOPIXEL
 };
 
 #ifdef __cplusplus
@@ -76,11 +76,11 @@ WEAK void SystemClock_Config(void)
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage 
+  /** Configure the main internal regulator output voltage
   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-  /** Initializes the CPU, AHB and APB busses clocks 
+  /** Initializes the CPU, AHB and APB busses clocks
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
@@ -91,11 +91,10 @@ WEAK void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLN = 100;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 3;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
-  /** Initializes the CPU, AHB and APB busses clocks 
+  /** Initializes the CPU, AHB and APB busses clocks
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -103,14 +102,12 @@ WEAK void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK) {
     Error_Handler();
   }
   PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_RTC;
   PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
-  {
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
     Error_Handler();
   }
 }
